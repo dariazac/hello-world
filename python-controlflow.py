01dad4af269f85e7f0e645e5bd9d686263094858
Daria Zachariassen <dariaskye16@gmail.com>
2018-09-21 21:50:27 +0200

    uncommented if elif else statement

--- python-controlflow.py
+++ python-controlflow.py
@@ -8,11 +8,11 @@ for i in range(1, 11):
 # 	print i
 # 	i += 1
 
-# a = 10
-# b = 20
-# if a < b:
-# 	print "{} is less than {}".format(a, b)
-# elif a == 20:
-# 	print "{} is equal to {}".format(a, b)
-# else:
-# 	print "{} is greater than {}".format(a, b)
+a = 10
+b = 20
+if a < b:
+    print "{} is less than {}".format(a, b)
+elif a == 20:
+    print "{} is equal to {}".format(a, b)
+else:
+    print "{} is greater than {}".format(a, b)
